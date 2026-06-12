f3f6a48f008d103be28fab6efb7e6f99300c06b7
Shree-2004 <shreelondhe2004@gmail.com>
2026-06-12 19:02:55 +0530

    fix api baseURL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL as string;
 
 const api = axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: API_URL,
   withCredentials: true,
 });
 
